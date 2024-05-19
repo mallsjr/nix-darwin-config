ab6f6da37dbc53d78fcf64ffc0c81c66a68adc2d
Michael Alls <allsjr@gmail.com>
2024-05-19 11:13:42 -0400

    zsh history settings

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -37,6 +37,19 @@
             rm -f -- "$tmp"
     }
 
+    # History
+    HISTSIZE=5000
+    HISTFILE=~/.zsh_history
+    SAVEHIST=$HISTSIZE
+    HISTDUP=erase
+    setopt appendhistory
+    setopt sharehistory
+    setopt hist_ignore_space
+    setopt hist_ignore_all_dups
+    setopt hist_save_no_dups
+    setopt hist_ignore_dups
+    setopt hist_find_no_dups
+
     source "${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
     source "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
     source "${pkgs.nix-zsh-completions}/share/zsh/plugins/nix/nix-zsh-completions.plugin.zsh"
@@ -47,6 +60,7 @@
     zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
     zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
     zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+    zstyle ':completion:*' menu no
   '';
   programs.zsh.plugins = [
     {
